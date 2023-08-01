0148b22a59715ffcfd374e2fd1cf9b03406665a3
Vinícius Ribeiro Fernandes <3655266+ViniciusVinna@users.noreply.github.com>
2023-07-31 22:17:53 -0300

    ✅ Add: account stubs test doubles

---
+++ core/use-cases/__data__/index.ts
@@ -0,0 +1,5 @@
+import { stubs } from "./stubs";
+
+export const scenarios = {
+  stubs,
+};
---
+++ core/use-cases/__data__/stubs/index.ts
@@ -0,0 +1,5 @@
+import accountsStub from "./accounts.stub.json";
+
+export const stubs = {
+  accounts: accountsStub as AccountBase[],
+};
